cd1a2b86f402e7a1ec189568f93588684b6b257a
krpk1900 <krpk1900@gmail.com>
2022-03-04 02:14:39 +0900

    [Add] 他のユーザーでも正常に動作するように修正

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 let star_elements = document.getElementsByClassName("star");
 let star_element = star_elements[0]
 star_element.addEventListener('click', function() {
-  chrome.tabs.query({active : true}, (tabs) => {
+  chrome.tabs.query({active : true, currentWindow:true}, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, {message : ["star", "num_star"]});
   });
 });
@@ -9,7 +9,7 @@ star_element.addEventListener('click', function() {
 let IE_elements = document.getElementsByClassName("IE");
 let IE_element = IE_elements[0]
 IE_element.addEventListener('click', function() {
-  chrome.tabs.query({active : true}, (tabs) => {
+  chrome.tabs.query({active : true, currentWindow:true}, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, {message : ["IE", "num_IE"]});
   });
 });
@@ -17,7 +17,7 @@ IE_element.addEventListener('click', function() {
 let good_elements = document.getElementsByClassName("good");
 let good_element = good_elements[0]
 good_element.addEventListener('click', function() {
-  chrome.tabs.query({active : true}, (tabs) => {
+  chrome.tabs.query({active : true, currentWindow:true}, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, {message : ["good", "num_good"]});
   });
 });
@@ -25,7 +25,7 @@ good_element.addEventListener('click', function() {
 let snow_elements = document.getElementsByClassName("snow");
 let snow_element = snow_elements[0]
 snow_element.addEventListener('click', function() {
-  chrome.tabs.query({active : true}, (tabs) => {
+  chrome.tabs.query({active : true, currentWindow:true}, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, {message : ["snow", "num_snow"]});
   });
 });
@@ -33,7 +33,7 @@ snow_element.addEventListener('click', function() {
 let moon_elements = document.getElementsByClassName("moon");
 let moon_element = moon_elements[0]
 moon_element.addEventListener('click', function() {
-  chrome.tabs.query({active : true}, (tabs) => {
+  chrome.tabs.query({active : true, currentWindow:true}, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, {message : ["moon", "num_moon"]});
   });
 });
@@ -41,7 +41,7 @@ moon_element.addEventListener('click', function() {
 let heart_elements = document.getElementsByClassName("heart");
 let heart_element = heart_elements[0]
 heart_element.addEventListener('click', function() {
-  chrome.tabs.query({active : true}, (tabs) => {
+  chrome.tabs.query({active : true, currentWindow:true}, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, {message : ["heart", "num_heart"]});
   });
 });
